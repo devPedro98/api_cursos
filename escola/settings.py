cb10e77bb1ffe68bc961909b4cf6d4b6b026af38
devPedro98 <pedroribeirobarrios@gmail.com>
2022-06-16 18:20:46 -0300

    Limitando o número de requisições com Throttling

--- escola/settings.py
+++ escola/settings.py
@@ -145,8 +145,16 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2
-}
+    'PAGE_SIZE': 2,
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ),
+    'DEFAULT_THROTTLE_RATES': { 
+        'anon': '5/minute',
+        'user': '10/minute'
+    }
+}   
