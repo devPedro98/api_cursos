dbb4f38798ecf4f97c3355a127681ab91c96b96c
devPedro98 <pedroribeirobarrios@gmail.com>
2022-06-17 11:53:35 -0300

    customizando a serialização dos dados

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg
 from rest_framework import serializers
 
 from .models import Avaliacao, Curso
@@ -23,13 +24,16 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
     def validate_avaliacao(self, valor):
         if valor in range(1, 6):
             return valor
-        raise serializers.ValidationError('A avaliação precisa ser um inteiro entre 1 e 5')
+        raise serializers.ValidationError(
+            'A avaliação precisa ser um inteiro entre 1 e 5')
+
 
 class CursoSerializer(serializers.ModelSerializer):
     # Nested Relationship
     # avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
     # HyperLinked Related Field
-    avaliacoes = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='avaliacao-detail') # noqa
+    avaliacoes = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='avaliacao-detail')  # noqa
+    media_avaliacoes = serializers.SerializerMethodField()
     # Primary Key Related Field
     # avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True) # noqa
 
@@ -41,5 +45,14 @@ class CursoSerializer(serializers.ModelSerializer):
             'url',
             'criacao',
             'ativo',
-            'avaliacoes'
+            'avaliacoes',
+            'media_avaliacoes'
         )
+
+    def get_media_avaliacoes(self, obj):
+        media = obj.avaliacoes.aggregate(
+            Avg('avaliacao')).get('avaliacao__avg')
+
+        if media is None:
+            return 0
+        return round(media * 2) / 2
